eed12718900d7133015a89ece44283572817b9fb
Adithya Ajith <adithya@satyaloka.org>
2024-11-16 12:05:29 -0600

    Prevent multiple identical windows from opening

--- app/contexts/ApplingManagerContext.tsx
+++ app/contexts/ApplingManagerContext.tsx
@@ -1,38 +1,61 @@
-import { createContext, Dispatch, FC, PropsWithChildren, ReactElement, SetStateAction, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  useState,
+} from "react";
 import { Appling } from "../types/Appling";
 import Window from "../components/Window";
 
 type ApplingManagerContextType = {
-  possibleApplings: Array<Appling>
-  openApplings: Array<Appling>
-  setOpenApplings: Dispatch<SetStateAction<Array<Appling>>>
-}
+  possibleApplings: Array<Appling>;
+  openApplings: Array<Appling>;
+  setOpenApplings: Dispatch<SetStateAction<Array<Appling>>>;
+};
 
 const ApplingsData: Array<Appling> = [
   {
     title: "Toolbox",
     icon: "/toolbox.png",
-    component: <Window close={() => { }} title="Toolbox" color="bg-coral"></Window>
+    component: (
+      <Window close={() => {}} title="Toolbox" color="bg-coral"></Window>
+    ),
   },
   {
     title: "Portfolio",
     icon: "/portfolio.png",
-    component: <Window close={() => { }} title="Toolbox" color="bg-coral"></Window>
-  }
-]
+    component: (
+      <Window close={() => {}} title="Toolbox" color="bg-coral"></Window>
+    ),
+  },
+];
 
-export const ApplingManagerContext = createContext<ApplingManagerContextType>({ openApplings: [], setOpenApplings: () => [], possibleApplings: ApplingsData });
+export const ApplingManagerContext = createContext<ApplingManagerContextType>({
+  openApplings: [],
+  setOpenApplings: () => [],
+  possibleApplings: ApplingsData,
+});
 
 type ApplingManagerContextProviderProps = {
-  initial?: Array<Appling>
-}
+  initial?: Array<Appling>;
+};
 
-export function ApplingManagerContextProvider({ children, initial = [] }: PropsWithChildren<ApplingManagerContextProviderProps>) {
+export function ApplingManagerContextProvider({
+  children,
+  initial = [],
+}: PropsWithChildren<ApplingManagerContextProviderProps>) {
   const [openApplings, setOpenApplings] = useState<Array<Appling>>(initial);
 
   return (
-    <ApplingManagerContext.Provider value={{ openApplings: openApplings, setOpenApplings: setOpenApplings, possibleApplings: ApplingsData }}>
+    <ApplingManagerContext.Provider
+      value={{
+        openApplings: openApplings,
+        setOpenApplings: setOpenApplings,
+        possibleApplings: ApplingsData,
+      }}
+    >
       {children}
     </ApplingManagerContext.Provider>
-  )
+  );
 }
