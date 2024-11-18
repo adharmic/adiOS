93d9feae20f1e4f245ff8473df967f3f40181d5c
Adithya Ajith <adithya@satyaloka.org>
2024-11-18 05:50:47 -0600

    Fix rounded start button on mobile

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -14,7 +14,7 @@ export default function MenuBar() {
 
   return (
     <div className="glass opacity-0 !bg-[#1621c9]/40 menuBar w-full translate-y-full flex justify-center sm:justify-start h-16 max-h-16">
-      <div className="absolute h-fit bg-valley rounded-full aspect-square flex items-center justify-center text-2xl border-white border p-2 top-1/2 -translate-y-1/2 sm:relative sm:h-full sm:rounded-[16px] sm:border-[rgba(0,0,0,0)] sm:border-r-white sm:rounded-r-none hover:cursor-pointer hover:saturate-150 transition-all">
+      <div className="absolute h-20 bg-valley rounded-full aspect-square flex items-center justify-center text-2xl border-white border p-2 top-1/2 -translate-y-1/2 sm:relative sm:h-full sm:rounded-[16px] sm:border-[rgba(0,0,0,0)] sm:border-r-white sm:rounded-r-none hover:cursor-pointer hover:saturate-150 transition-all">
         Start
       </div>
       <div className="pl-2 hidden md:flex items-center gap-2">
@@ -29,7 +29,7 @@ export default function MenuBar() {
           );
         })}
       </div>
-      <div className="ml-auto h-full items-center justify-center p-4 rounded-[16px] border-[rgba(255,255,255,0.3)] border-l !rounded-l-none !bg-[#fff]/20 hidden md:flex">
+      <div className="ml-auto h-full items-center justify-center p-4 rounded-[16px] border-[rgba(255,255,255,0.3)] border-l !rounded-l-none !bg-[#fff]/20 md:flex">
         <h1>{date.toDateString()}</h1>
       </div>
     </div>
