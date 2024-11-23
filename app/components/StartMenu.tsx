c3932e906ac0a1e0ec1b2dafb1b092a7d0d91271
Adithya Ajith <adithya@satyaloka.org>
2024-11-23 13:42:39 -0600

    Add link to GitHub in Start Menu

--- app/components/StartMenu.tsx
+++ app/components/StartMenu.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useEffect, useRef } from "react";
 
 type StartMenuProps = {
@@ -32,13 +33,27 @@ export default function StartMenu({ isHidden, setIsHidden }: StartMenuProps) {
             : "flex flex-col menuBarInverted justify-between items-center")
         }
       >
-        <div className="p-6 menuBar w-full self-start">Welcome, Guest</div>
+        <div className="p-6 menuBar w-full self-start text-3xl">
+          Welcome, Guest
+        </div>
         <div className="h-full w-full pl-1 pr-1 flex">
           <div className="bg-sand h-full w-full" />
           <div className="bg-background h-full w-3/4 border-l-2 border-cobalt" />
         </div>
-        <div className="menuBar w-full p-6 flex items-center justify-end">
-          Choose an option
+        <div className="menuBar w-full p-6 flex items-center justify-end gap-4 text-xl">
+          <a
+            href="https://github.com/adharmic/"
+            target="_blank"
+            className="bg-foreground rounded-full h-12 w-12 border border-background"
+          >
+            <Image
+              src="/github.svg"
+              alt="Link to my GitHub profile"
+              height={48}
+              width={48}
+            />
+          </a>
+          GitHub{" "}
         </div>
       </div>
     </div>
