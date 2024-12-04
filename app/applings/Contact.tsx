6de44cc12d6401e7f95eef803db27f527715a657
Adithya Ajith <adithya@satyaloka.org>
2024-12-04 07:48:13 -0600

    Add Contact form mockup

--- app/applings/Contact.tsx
+++ app/applings/Contact.tsx
@@ -1,3 +1,21 @@
 export default function Contact() {
-  return <div>Contact Me</div>;
+  return (
+    <div className="w-[600px] max-w-full max-h-full text-black flex flex-col gap-4">
+      <div className="flex flex-col">
+        <label className="font-bold text-xl">Name</label>
+        <input className="border border-cobalt p-2"></input>
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold text-xl">E-mail</label>
+        <input className="border border-cobalt p-2"></input>
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold text-xl">Subject</label>
+        <textarea className="border border-cobalt p-2"></textarea>
+      </div>
+      <button className="menuBar p-2 w-fit text-white font-bold">
+        Submit!
+      </button>
+    </div>
+  );
 }
